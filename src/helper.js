a6761e63ad234ec44f9e70a56db9ccff917eeee0
henryleu <henryleu@126.com>
2018-01-15 11:42:11 +0800

    add helper for pulling metadata

---
+++ src/helper.js
@@ -0,0 +1,15 @@
+/*
+ * Created by Henry Leu (henryleu@126.com) on 2018/1/15
+ */
+const jsonFile = require('jsonfile');
+
+module.exports = function (filePath) {
+    return function (json) {
+        return new Promise(function (resolve, reject) {
+            jsonFile.writeFile(filePath, json, {spaces: 4}, function (err) {
+                if (err) return reject(err);
+                resolve(json);
+            });
+        });
+    };
+};
